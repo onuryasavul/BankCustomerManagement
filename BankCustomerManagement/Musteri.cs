a7fc01c8c261423ce338e5f3dbb8c8414995d85f
ONUR YASAVUL <onur.yasavul@std.yildiz.edu.tr>
2022-07-05 10:25:01 +0300

    Proje dosyası ekle.

---
+++ BankCustomerManagement/Musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankCustomerManagement
+{
+    internal class Musteri
+    {
+        public int id { get; set; }
+        public string isim { get; set; }
+        public string soyIsim { get; set; }
+        public double bakiye { get; set; }
+
+    }
+}
